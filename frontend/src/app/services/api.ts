16600135518e01431df78cd689aeb0f7082bc38e
Matías <matias1garcia2@gmail.com>
2026-05-24 22:58:59 -0300

    testerv2

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -3,6 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
